e88c4fb93871329b86ee3a50c6a36aacfa30dd71
ailento <ailentonani@gmail.com>
2017-04-26 21:06:19 -0300

    agreagar action

---
+++ src/utn/frd/action/PersonaAction.java
@@ -0,0 +1,14 @@
+package utn.frd.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class PersonaAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+
+	//private MessageStore messageStore;
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
